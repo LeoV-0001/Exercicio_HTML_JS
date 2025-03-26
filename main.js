ad9495d5abdf946d01e1aecde56940e1e757741c
Leonardo Victor <116875038+LeoV-0001@users.noreply.github.com>
2025-03-26 15:23:27 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('campos');
+const valor1 = document.getElementById('valor1')
+const valor2 = document.getElementById('valor2')
+
+const mensagemCerto = `O valor B é maior que o valor A`
+const mensagemErrado = `O valor B não é maior que o valor A`
+
+function comparaValor () {
+    compara = valor1.value > valor2.value;
+    return compara
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+if (valor1.value > valor2.value) {
+    document.querySelector('#mensagem').innerHTML = mensagemErrado;
+} 
+else {
+    document.querySelector('#mensagem').innerHTML = mensagemCerto;
+    }
+
+})
